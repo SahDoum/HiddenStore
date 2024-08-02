0b9ff1cfa4f5235fa3f7410164a6209b12f1d82b
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-08-03 02:19:13 +0300

    move db password to .env; created .env.template

--- apps/server/config.py
+++ apps/server/config.py
@@ -2,3 +2,6 @@ import os
 
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
+
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD", "postgres")
+DATABASE_URL = f"postgresql+asyncpg://postgres:{DATABASE_PASSWORD}@db/db" 
--- apps/server/db_config.py
+++ apps/server/db_config.py
@@ -1,23 +1,10 @@
-# import pydantic.json
-# import json
-
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-import pydantic.json
-import json
-
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@db/db"  # os.environ.get("DATABASE_URL")
+from config import DATABASE_URL
 
 
-# def _custom_json_serializer(*args, **kwargs) -> str:
-#     """
-#     Encodes json in the same way that pydantic does.
-#     """
-#     return json.dumps(*args, default=pydantic.json.pydantic_encoder, **kwargs)
-
-
-engine = create_async_engine(DATABASE_URL, echo=True, future=True) # , json_serializer=_custom_json_serializer)
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_session() -> AsyncSession:
