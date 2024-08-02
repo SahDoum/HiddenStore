e1b66bd92be234679165fc88609c79268123bfaf
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-08-03 01:31:43 +0300

    enable telegram initData at cafe js objects

--- frontend/static/scripts/main.js
+++ frontend/static/scripts/main.js
@@ -7,11 +7,4 @@ import { initRipple } from './packages/rippleEffect.js';
 setupTelegramWebAppEvents();
 initRipple();
 
-// Initialize Cafe with options
-const cafe = new Cafe();
-cafe.init({
-	apiUrl: `${window.location.origin}/api`,
-	userId: 0, // Replace with actual user ID if available
-	initDataHash: '',
-	dataCheckString: ''
-});
+window.Cafe = Cafe;
