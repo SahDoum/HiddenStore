7a66170c11f2e9cf1ab4f931660f1e52925c3c6f
Aleksandr Dumarevskii <sahdoum@gmail.com>
2024-08-05 18:29:19 +0300

    add keyboard to /start command

--- apps/customer_bot/handlers/commands.py
+++ apps/customer_bot/handlers/commands.py
@@ -3,76 +3,97 @@ from aiogram import types
 from init import dp
 from templates.messages import MESSAGES
 from init import render_template
-from libs.hidden_client import HiddenUser, HiddenOrder, HiddenMenu, HiddenItem, OrderItem
+from libs.hidden_client import (
+    HiddenUser,
+    HiddenOrder,
+    HiddenMenu,
+    HiddenItem,
+    OrderItem,
+)
 
-from keyboards import menu_keyboard
+from keyboards import menu_keyboard, start_keyboard
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-@dp.message_handler(commands='start')
+@dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
-    await message.reply(MESSAGES['start'])
+    telegram_id = str(message.from_user.id)
+    username = None
+    if message.from_user.username:
+        username = message.from_user.username
+    elif message.from_user.first_name:
+        username = message.from_user.first_name
+
+    await HiddenUser.get_or_create(telegram_id=telegram_id, name=username)
+    await message.reply(MESSAGES["start"], reply_markup=start_keyboard())
 
 
-@dp.message_handler(commands='about')
+@dp.message_handler(commands="about")
 async def cmd_about(message: types.Message):
-    telegram_id = str(message.from_user.id)    
+    telegram_id = str(message.from_user.id)
     user = await HiddenUser.get_or_create(telegram_id=telegram_id)
     if not user:
-        await message.reply(MESSAGES['error_fetching_user'])
+        await message.reply(MESSAGES["error_fetching_user"])
         return
-    msg = render_template('user_info.txt', user=user.user)
+    msg = render_template("user_info.txt", user=user.user)
     await message.reply(msg)
-        
 
-@dp.message_handler(commands='order')
+
+@dp.message_handler(commands="order")
 async def cmd_order(message: types.Message):
     telegram_id = str(message.from_user.id)
     user = await HiddenUser.get_or_create(telegram_id=telegram_id)
     if not user:
-        await message.reply(MESSAGES['error_fetching_user'])
+        await message.reply(MESSAGES["error_fetching_user"])
         return
     items = [
-        (OrderItem(item="Item1", count=1, details="Some details", price=100, unit="pcs"), 1.0),
+        (
+            OrderItem(
+                item="Item1", count=1, details="Some details", price=100, unit="pcs"
+            ),
+            1.0,
+        ),
     ]
     order = await HiddenOrder.create(items=items, price=100, user=user)
-    await message.reply(MESSAGES['order_success'].format(order_id=order.order.id))
+    await message.reply(MESSAGES["order_success"].format(order_id=order.order.id))
 
 
-@dp.message_handler(commands='orders')
+@dp.message_handler(commands="orders")
 async def cmd_orders(message: types.Message):
     telegram_id = str(message.from_user.id)
     user = await HiddenUser.get_or_create(telegram_id=telegram_id)
     if not user:
-        await message.reply(MESSAGES['error_fetching_user'])
+        await message.reply(MESSAGES["error_fetching_user"])
         return
 
     orders = await user.get_orders()
     if isinstance(orders, list):
         for order in orders:
             hidden_order = HiddenOrder(order, user)
-            msg = render_template('order_info.txt', order=hidden_order.order, items=hidden_order.items())
+            msg = render_template(
+                "order_info.txt", order=hidden_order.order, items=hidden_order.items()
+            )
             await message.reply(msg)
             return
 
-    await message.reply(MESSAGES['error_orders'])
+    await message.reply(MESSAGES["error_orders"])
 
 
-@dp.message_handler(commands='menu')
+@dp.message_handler(commands="menu")
 async def cmd_menu(message: types.Message):
     menu = await HiddenMenu.get_items()
     if len(menu.hidden_items) > 0:
         menu_items = menu.items()
         keyboard = menu_keyboard(menu_items)
-        msg = render_template('menu.txt', items=menu_items)
+        msg = render_template("menu.txt", items=menu_items)
         await message.reply(msg, reply_markup=keyboard)
     else:
-        await message.reply(MESSAGES['error_fetching_menu'])
+        await message.reply(MESSAGES["error_fetching_menu"])
 
 
-@dp.message_handler(commands='create')
+@dp.message_handler(commands="create")
 async def cmd_create(message: types.Message):
     try:
         # Parse command arguments
@@ -80,7 +101,7 @@ async def cmd_create(message: types.Message):
         price = int(price_str)
 
         created_item = await HiddenItem.create(item_str, details_str, price, unit_str)
-        
+
         # Respond to the user
         await message.reply(
             f"Item created successfully!\n"
@@ -91,16 +112,17 @@ async def cmd_create(message: types.Message):
             f"Unit: {created_item.item.unit}",
         )
     except ValueError:
-        await message.reply("Invalid input. Please make sure to use the correct format: /create {item} {details} {price} {unit}.")
+        await message.reply(
+            "Invalid input. Please make sure to use the correct format: /create {item} {details} {price} {unit}."
+        )
     except Exception as e:
         await message.reply(f"An error occurred: {e}")
 
 
-
 def register_handlers():
-    dp.register_message_handler(cmd_start, commands='start')
-    dp.register_message_handler(cmd_about, commands='about')
-    dp.register_message_handler(cmd_order, commands='order')
-    dp.register_message_handler(cmd_orders, commands='orders')
-    dp.register_message_handler(cmd_menu, commands='menu')
-    dp.register_message_handler(cmd_create, commands='create')
+    dp.register_message_handler(cmd_start, commands="start")
+    dp.register_message_handler(cmd_about, commands="about")
+    dp.register_message_handler(cmd_order, commands="order")
+    dp.register_message_handler(cmd_orders, commands="orders")
+    dp.register_message_handler(cmd_menu, commands="menu")
+    dp.register_message_handler(cmd_create, commands="create")
--- apps/customer_bot/keyboards.py
+++ apps/customer_bot/keyboards.py
@@ -1,5 +1,6 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from libs.models.models import OrderItem
+from config import WEBAPP_URL
 
 
 def menu_keyboard(items: list[OrderItem]):
@@ -8,5 +9,13 @@ def menu_keyboard(items: list[OrderItem]):
     for item in items:
         button = InlineKeyboardButton(text=item.item, callback_data=f"order_{item.id}")
         keyboard.add(button)
+
+    return keyboard
+
+
+def start_keyboard():
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton(text="Заказать", web_app=WebAppInfo(url=WEBAPP_URL))
+    keyboard.add(button)
 
     return keyboard
